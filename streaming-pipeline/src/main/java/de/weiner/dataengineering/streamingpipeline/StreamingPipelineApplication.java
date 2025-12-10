3c4095e5c5b40ac539c789d910233d2246547384
Daniel W <deniel98045@gmail.com>
2025-12-10 10:24:31 +0100

    init

---
+++ streaming-pipeline/src/main/java/de/weiner/dataengineering/streamingpipeline/StreamingPipelineApplication.java
@@ -0,0 +1,13 @@
+package de.weiner.dataengineering.streamingpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingPipelineApplication.class, args);
+	}
+
+}
---
+++ streaming-pipeline/src/test/java/de/weiner/dataengineering/streamingpipeline/StreamingPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package de.weiner.dataengineering.streamingpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
